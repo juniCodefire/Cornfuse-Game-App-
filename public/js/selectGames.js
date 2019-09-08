1e7753913b57c45c88389ae0e7eeb7170d619423
Mr-Juni <junipreach2017@gmail.com>
2019-09-08 13:18:27 +0100

    ye

--- public/js/selectGames.js
+++ public/js/selectGames.js
@@ -1,17 +1,29 @@
 const games = Array.from(document.querySelectorAll(".games"));
+const currentBg_de = document.querySelector(".slick-current img");
 
 const repeatAnim = () => {
 	console.log('p')
 	//Use the current zoom out class to get it;s image
     const currentBg = document.querySelector(".slick-current img");
+    console.log(currentBg)
     //get the website background
     const gameBg = document.querySelector("[data-game-bg]")
     //add fadeIn to the background
     gameBg.classList.add("animated", "fadeIn")
     //replace the background
     gameBg.style.backgroundImage = `linear-gradient(rgba(0,0,0,0.7), rgba(0,0,0,0.8)),url('${currentBg.src}')`;
+
 }
 //repeat interval depending on the slide speed to simulate flow process
-setInterval(() => {
+const switchBg = setInterval(() => {
     repeatAnim()
 }, 4000);
+
+// //Add a hover event to show play btn
+
+// const showPlayBtn = () => {
+// 	clearInterval(switchBg);
+// 	currentBg_de.
+
+// }
+// showPlayBtn.addEventListener('hover', (event))
